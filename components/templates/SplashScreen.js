c22fc4586c660dfe91f72f5fc45c3cb2fefca961
Sajika1 <sajjadsajjad464@gmail.com>
2023-03-24 19:08:42 +0430

    SPLASHSCREEN | change animated

--- components/templates/SplashScreen.js
+++ components/templates/SplashScreen.js
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 // logo
 import { UilStreering } from "@iconscout/react-unicons";
 
+import { motion } from "framer-motion";
+
 export default function SplashScreen({ setIsMountedFinish }) {
 	useEffect(() => {
 		const timeOut = setTimeout(() => {
@@ -13,12 +15,18 @@ export default function SplashScreen({ setIsMountedFinish }) {
 
 	return (
 		<section className="w-screen h-screen flex items-center justify-center">
-			<div className="flex items-center gap-2 animate-bounce">
+			<motion.div
+				initial={{ y: 30 }}
+				animate={{ y: 0 }}
+				exit={{ y: -30 }}
+				transition={{ repeat: Infinity, duration: 1.5, type: "spring" }}
+				className="flex items-center gap-2"
+			>
 				<div className="w-16 h-16">
 					<UilStreering className="text-primary w-full h-full" />
 				</div>
 				<h1 className="text-5xl font-semibold tracking-wide">carline</h1>
-			</div>
+			</motion.div>
 		</section>
 	);
 }
